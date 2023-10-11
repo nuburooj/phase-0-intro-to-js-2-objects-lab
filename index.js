20a32c21d5eecbc5792dc121240d8737af14e967
nuburooj <nuburoojkhattak@gmail.com>
2023-10-10 22:00:45 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution in this file!
+const employee = {};
+
+employee.name = "Chavo";
+
+employee.streetAddress = "2909 albemarle rd, Brooklyn, NY, 11226";
+
+employee;
+
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+
+    const newEmployee = { ...employee };
+
+    newEmployee[key] = value;
+    return employee, newEmployee;
+
+}
+
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+   
+    employee[key] = value;
+    return employee;
+
+}
+
+
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+
+    delete employee.name;
+    return employee;
+
+}
+
+
+
+function deleteFromEmployeeByKey(employee, key){
+    
+    const newEmployee = {...employee};
+    delete newEmployee.name;
+    return newEmployee;
+    
+}
